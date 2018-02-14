41145775aa0080c5a4a7f1cd5ae77279fd7c43a4
MJ Rossetti <michael.rossetti@gsa.gov>
2018-02-14 16:25:09 -0500

    Not working

--- my_app.go
+++ my_app.go
@@ -7,9 +7,15 @@ import (
 
 func main (){
   var app = express.Express()
+
   app.Get("/", func(req *request.Request, res *response.Response, next func()){
     res.Write("Hello World")
     // you can skip closing connection
   })
+
+  app.Get("/hello", func(req *request.Request, res *response.Response, next func()){
+	   res.SendFile("./views/hello.html", false)
+  })
+
   app.Start("8080")
 }
